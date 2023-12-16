e7aac5109e10e4d4298501873c2fe65002d2ca64
Foxyneesan <limonka.lima@interia.pl>
2023-12-16 10:35:06 +0100

    DodaniePausemenu

---
+++ Nightmare_Friday_on_Halloween/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+  [SerializeField]  public GameObject pauseMenuUI;
+    public GameObject gameplayUI;
+
+    private bool isPaused = false;
+
+	void Start()
+	{
+		 ResumeGame();
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+        gameplayUI.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        gameplayUI.SetActive(true);
+    }
+}
+
