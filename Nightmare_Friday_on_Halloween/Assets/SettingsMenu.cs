d8e2f145f88b2ace8500a4af11dd2a48aa07d7d8
Foxyneesan <limonka.lima@interia.pl>
2024-02-14 19:45:54 +0100

    Porządki

--- Nightmare_Friday_on_Halloween/Assets/SettingsMenu.cs
+++ Nightmare_Friday_on_Halloween/Assets/SettingsMenu.cs
@@ -2,18 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
 	public AudioMixer audioMixer;
+	[SerializeField] public Slider musicSlider;
+	[SerializeField] public Slider sfxSlider;	
+
+	public void Start()
+	{
+
+		if (PlayerPrefs.HasKey("musicVolume"))
+		{
+		LoadVolume();
+		}
+
+		else
+		{
+		SetMusicVolume();
+		SetSFXVolume();
+		}
 	
-  public void SetVolume (float volume)
-{
+	}
+
+  	public void SetMusicVolume ()
+	{
+	float volume = musicSlider.value;
 	audioMixer.SetFloat("Music", volume);
+	
+
+	PlayerPrefs.SetFloat("musicVolume", volume);
+	
+	}
+
+	public void SetSFXVolume ()
+	{
+	float volume = sfxSlider.value;
 	audioMixer.SetFloat("SFX", volume);
 	
-}
 
+	PlayerPrefs.SetFloat("SFXVolume", volume);
+	
+	}
+
+	private void LoadVolume()
+	{
+	musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+	sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+	SetMusicVolume();
+	SetSFXVolume();
+	}
 
 }
 
