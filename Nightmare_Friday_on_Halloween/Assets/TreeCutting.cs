fb71501a68c6ac134fdb5e886fe6ed956ab29f40
Bulciaa <julia.kalemba@protonmail.com>
2024-02-23 22:08:22 +0100

    wyświetlanie klawiszy które gracz ma klikać
    zmniejszanie i zwiększanie po kliknięciu odpowiednich klawiszy (A i D)

--- Nightmare_Friday_on_Halloween/Assets/TreeCutting.cs
+++ Nightmare_Friday_on_Halloween/Assets/TreeCutting.cs
@@ -9,19 +9,32 @@ public class TreeCutting : MonoBehaviour
 {
     public TMP_Text scoreText;
     public TMP_Text timeText;
+    public GameObject buttonA;
+    public GameObject buttonD;
+
     private int score = 0;
     private bool lastKeyPressWasA = false;
     private float gameTime = 10f; // Czas gry w sekundach
     private bool gameRunning = true;
 
+    private Vector3 originalSizeA;
+    private Vector3 originalSizeD;
     void Start()
     {
+        originalSizeA = buttonA.transform.localScale;
+        originalSizeD = buttonD.transform.localScale;
+
+        // Ustawienie kwadracika A wiêkszego ni¿ D na pocz¹tku gry
+        buttonA.transform.localScale = originalSizeA * 1.2f;
+
+
         UpdateScoreText();
         UpdateTimeText();
     }
 
     void Update()
     {
+
         if (gameRunning)
         {
             gameTime -= Time.deltaTime;
@@ -43,16 +56,30 @@ public class TreeCutting : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.A) && !lastKeyPressWasA)
             {
+                DecreaseSize(buttonA);
+                IncreaseSize(buttonD);
                 IncreaseScore();
                 lastKeyPressWasA = true;
             }
             else if (Input.GetKeyDown(KeyCode.D) && lastKeyPressWasA)
             {
+                DecreaseSize(buttonD);
+                IncreaseSize(buttonA);
                 IncreaseScore();
                 lastKeyPressWasA = false;
             }
         }
     }
+    void IncreaseSize(GameObject button)
+    {
+        button.transform.localScale = originalSizeA * 1.2f;
+    }
+
+    void DecreaseSize(GameObject button)
+    {
+        button.transform.localScale = originalSizeA * 0.8f;
+    }
+
 
     void IncreaseScore()
     {
