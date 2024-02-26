45bde074cc5b8804f602ce7f72f93cc93a8b52d4
Bulciaa <julia.kalemba@protonmail.com>
2024-02-26 22:48:54 +0100

    time.timescale = 0 -> dodanie tylko tej linijki :)

--- Nightmare_Friday_on_Halloween/Assets/ExitTutorial.cs
+++ Nightmare_Friday_on_Halloween/Assets/ExitTutorial.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ExitTutorial : MonoBehaviour
 {
     public GameObject TutorialCanvas;
+    public TreeCutting treeCuttingGame; // Referencja do skryptu zarz¹dzaj¹cego gr¹
+
     public void ExitTutorialCanvas()
     {
         TutorialCanvas.SetActive(false);
