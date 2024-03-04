b04274423df792fd7b9ba3e4269b81b6c9cf8dae
Bulciaa <julia.kalemba@protonmail.com>
2024-03-04 01:38:33 +0100

    Canvas z napisem "Find Krueger" + jego animacja

--- Nightmare_Friday_on_Halloween/Assets/ExitTutorialWSAD.cs
+++ Nightmare_Friday_on_Halloween/Assets/ExitTutorialWSAD.cs
@@ -7,15 +7,28 @@ public class ExitTutorialWSAD : MonoBehaviour
     public GameObject TutorialCanvas;
     public PlayerController playerController;
 
+    public GameObject FindText;
+
     private bool tutorialActive = true;
     private void Start()
     {
+        StartCoroutine(ShowTutorialSequence());
+
         if (tutorialActive)
         {
             Time.timeScale = 0; 
         }
     }
+    IEnumerator ShowTutorialSequence()
+    {
+        // Poka¿ animacjê "Find"
+        FindText.SetActive(true);
+        yield return new WaitForSeconds(6f); // Czas trwania animacji "Find"
 
+        // Poka¿ "CanvasTutorial" i wy³¹cz "Find"
+        FindText.SetActive(false);
+        TutorialCanvas.SetActive(true);
+    }
     public void ExitTutorialCanvas()
     {
         TutorialCanvas.SetActive(false);
